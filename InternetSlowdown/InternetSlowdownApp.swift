f38b079024bce69fb5f9e8a0906d57caed5678d5
mateo <mateo.ochoac@gmail.com>
2022-11-22 13:40:04 -0400

    Initial Commit

---
+++ InternetSlowdown/InternetSlowdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternetSlowdownApp.swift
+//  InternetSlowdown
+//
+//  Created by Mateo Ochoa on 2022-11-22.
+//
+
+import SwiftUI
+
+@main
+struct InternetSlowdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
